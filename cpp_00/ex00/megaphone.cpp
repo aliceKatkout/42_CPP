04f0f541e82691cc3ebf34d8e3c08331a53890e3
Alice VEDRENNE <avedrenn@paul-f4Ar8s5.clusters.42paris.fr>
2023-09-21 17:26:41 +0200

    CPP_00, add ex00 :tada:

---
+++ cpp_00/ex00/megaphone.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: avedrenn <avedrenn@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/21 17:21:55 by avedrenn          #+#    #+#             */
+/*   Updated: 2023/09/21 17:21:59 by avedrenn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return 0;
+	}
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			for (int j = 0; argv[i][j]; j++)
+			{
+				std::cout << (char)std::toupper(argv[i][j]);
+			}
+		}
+		return 0;
+	}
+}
