6bb64a78b0685923f77aa6ce6a694140ad4e2163
Alice VEDRENNE <avedrenn@paul-f3Br3s3.clusters.42paris.fr>
2023-10-09 16:36:27 +0200

    Start ex01 :bug:

---
+++ 02/ex01/Fixed.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: avedrenn <avedrenn@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/09 15:16:06 by avedrenn          #+#    #+#             */
+/*   Updated: 2023/10/09 16:35:23 by avedrenn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+
+class Fixed
+{
+	public:
+		Fixed(const int i);
+		~Fixed();
+
+		Fixed	(const Fixed &obj);
+
+		Fixed&	operator=(const Fixed&);
+
+		int		getRawBits(void) const;
+		void	setRawBits(int const raw);
+
+	private:
+		int					_nb;
+		const static int	_static_nb = 8;
+};
+
+
+#endif
---
+++ 02/ex01/main.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: avedrenn <avedrenn@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/09 15:15:14 by avedrenn          #+#    #+#             */
+/*   Updated: 2023/10/09 16:34:16 by avedrenn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+std::ostream	&operator<<(std::ostream os, Fixed &f)
+{
+	return (os << f.)
+}
+
+int main( void )
+{
+	Fixed a;
+	Fixed const b( 10 );
+	Fixed const c( 42.42f );
+	Fixed const d( b );
+	a = Fixed( 1234.4321f );
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+	return 0;
+}
