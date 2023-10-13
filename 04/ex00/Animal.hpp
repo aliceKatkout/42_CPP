46a4dc645ea9bcedbd6675765d978ad6f6022f24
Alice VEDRENNE <avedrenn@made-f0Br7s15.clusters.42paris.fr>
2023-10-13 17:08:19 +0200

    Start 04, add ex00 :tada:

---
+++ 04/ex00/Animal.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: avedrenn <avedrenn@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/13 15:48:22 by avedrenn          #+#    #+#             */
+/*   Updated: 2023/10/13 16:57:13 by avedrenn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ANIMAL_HPP
+# define ANIMAL_HPP
+
+#include <iostream>
+
+class Animal
+{
+	public :
+		Animal();
+		Animal(Animal &other);
+		virtual ~Animal();
+
+		Animal	&operator=(const Animal &other);
+
+		std::string		getType() const;
+		virtual void	makeSound() const;
+
+	protected :
+		std::string		_type;
+};
+
+#endif
---
+++ 04/ex00/Cat.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Cat.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: avedrenn <avedrenn@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/13 15:47:55 by avedrenn          #+#    #+#             */
+/*   Updated: 2023/10/13 15:58:31 by avedrenn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Cat.hpp"
+
+Cat::Cat() : Animal() {
+	_type = "Cat";
+	std::cout << "A Cat is born." << std::endl;
+};
+
+Cat::Cat(Cat &other) {
+	(*this) = other;
+	std::cout << "A Cat is born, cloned." << std::endl;
+}
+
+Cat::~Cat() {
+	std::cout << "A Cat is dead." << std::endl;
+}
+
+Cat	&Cat::operator=(const Cat &other) {
+	_type = other._type;
+	return (*this);
+};
+
+void	Cat::makeSound() const {
+	std::cout << _type << " : Miaou" << std::endl;
+}
