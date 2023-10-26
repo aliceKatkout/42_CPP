bd1c26f6173312068d085509fae3fbc3eb775d1a
Alice VEDRENNE <avedrenn@made-f0Cr4s1.clusters.42paris.fr>
2023-10-26 18:56:09 +0200

    WIP 06

--- 06/ex00/ScalarConverter.cpp
+++ 06/ex00/ScalarConverter.cpp
@@ -6,65 +6,40 @@
 /*   By: avedrenn <avedrenn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 15:56:17 by avedrenn          #+#    #+#             */
-/*   Updated: 2023/10/25 18:00:57 by avedrenn         ###   ########.fr       */
+/*   Updated: 2023/10/26 17:14:22 by avedrenn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 
 ScalarConverter::ScalarConverter() : _str("0") {};
-ScalarConverter::ScalarConverter( ScalarConverter const & src ) { *this = src; };
+ScalarConverter::ScalarConverter( ScalarConverter const &src ) { *this = src; };
 ScalarConverter::~ScalarConverter( void ) {};
 
-ScalarConverter &	ScalarConverter::operator=( ScalarConverter const & src )
+ScalarConverter &	ScalarConverter::operator=( ScalarConverter const &src )
 {
 	(void) src;
 	return *this;
 }
 
-void				ScalarConverter::convert( const std::string &str )
+
+
+bool isNumeric(std::string const &str)
 {
+    return !str.empty() && str.find_first_not_of("0123456789") == std::string::npos;
+}
+
+int		ScalarConverter::getType() {
+
+}
+
+void	ScalarConverter::convert( const std::string &str )
+{
+	int	type;
+
 	_str = str;
-	
-	std::cout << "char: ";
-	try
-	{
-		char c = static_cast<char>(std::stoi(str));
-		if (c < 32 || c > 126)
-			throw NonDisplayableException();
-		std::cout << "'" << c << "'" << std::endl;
-	}
-	catch (std::exception &e)
-	{
-		std::cout << e.what() << std::endl;
-	}
-	std::cout << "int: ";
-	try
-	{
-		std::cout << static_cast<int>(std::stoi(str)) << std::endl;
-	}
-	catch (std::exception &e)
-	{
-		std::cout << e.what() << std::endl;
-	}
-	std::cout << "float: ";
-	try
-	{
-		std::cout << static_cast<float>(std::stof(str)) << "f" << std::endl;
-	}
-	catch (std::exception &e)
-	{
-		std::cout << e.what() << std::endl;
-	}
-	std::cout << "double: ";
-	try
-	{
-		std::cout << static_cast<double>(std::stod(str)) << std::endl;
-	}
-	catch (std::exception &e)
-	{
-		std::cout << e.what() << std::endl;
-	}
+	type = getType();
+
 }
 
 const char *		ScalarConverter::ImpossibleException::what( void ) const throw()
--- 06/ex00/ScalarConverter.hpp
+++ 06/ex00/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: avedrenn <avedrenn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 15:54:50 by avedrenn          #+#    #+#             */
-/*   Updated: 2023/10/25 17:45:49 by avedrenn         ###   ########.fr       */
+/*   Updated: 2023/10/26 17:15:29 by avedrenn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,44 @@
 # define SCALARCONVERTER_HPP
 
 # include <iostream>
+# include <string>
+# include <algorithm>
+
+enum Types { error, char, int, float, double };
+
+bool isNumeric(std::string const &str);
 
 class ScalarConverter
 {
 	public:
 		ScalarConverter();
-		ScalarConverter( ScalarConverter const & );
+		ScalarConverter( ScalarConverter const &src );
 		~ScalarConverter();
 
-		ScalarConverter &	operator=( ScalarConverter const& );
+		ScalarConverter &	operator=( ScalarConverter const &src );
 
 		void				convert( const std::string &str );
 
 		class ImpossibleException : public std::exception
 		{
 			public:
-				virtual const char *	what( void ) const throw();
+				virtual const char *	what() const throw();
 		};
 
 		class NonDisplayableException : public std::exception
 		{
 			public:
-				virtual const char *	what( void ) const throw();
+				virtual const char *	what() const throw();
 		};
 
 	private:
-		const std::string		_str;
+		const std::string	_str;
+		int					getType();
+
+		char				toChar();
+		int					toInt();
+		float				toFloat();
+		double				toDouble();
 };
 
 #endif
